5f14279e120f70c253753695786a58613587c367
SAli <set.ali.mousavi@gmail.com>
2022-12-01 16:58:22 +0330

    before eject

---
+++ 5-counter-react-webpack/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Counter from './Counter'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div>
+        <h2>Counter App</h2>
+        <Counter />
+        <Counter />
+        <Counter />
+      </div>
+    )
+  }
+}
---
+++ 5-counter-react-webpack/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+const rootContainer = document.getElementById('root')
+
+const root = ReactDOM.createRoot(rootContainer)
+
+root.render(<App />)
